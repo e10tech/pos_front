abb6f69d95a4b3b0c38d6ad881ded5b3cc7a5176
e10tech <e10fortech@gmail.com>
2025-09-28 14:16:26 +0900

    初回デプロイ

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
 };
 
 export default nextConfig;
